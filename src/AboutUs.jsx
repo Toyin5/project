e84705cb46c9213066ed6131a7903dae30395479
toyin <toyinmuhammed50@gmail.com>
2022-11-19 12:06:38 +0200

    Initial commit

---
+++ src/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function AboutUs() {
+    const [data, setData] = useState("")
+
+
+    return (
+        <div>
+            <h1>This is the about us component</h1>
+            <p>Now we will import the profiles below</p>
+        </div>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import AboutUs from './AboutUs'
+
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <AboutUs />
+
+    </div>
+  )
+}
+
+export default App
